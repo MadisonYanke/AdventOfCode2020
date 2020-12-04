994f31bb8dea921a8782c3f603abfab1b06b5463
Tyler Yanke <yanketyler@gmail.com>
2020-12-04 13:49:52 -0500

    Add files via upload

--- day4.py
+++ day4.py
@@ -1,28 +1,21 @@
 import re
 
-def CheckPassports(validFields, passportList, validate=False):
+def CheckPassports(validFields, passportList, rules = None, validate=False):
     validPassports = 0
     for passport in passportList:
-        validPassport = True
-        for field in validFields:
-                if field not in (i[0] for i in passport):
-                    validPassport = False
-        if validate and validPassport:
-            validPassport = ValidatePassport(passport)
+        passportFields = [i[0] for i in passport]
+        validPassport = set(validFields).issubset(passportFields)
+        if validate and validPassport: validPassport = ValidatePassport(passport, rules)
         if validPassport:
             validPassports += 1
-            print(passport)
     return validPassports
 
-def ValidatePassport(passport):
+def ValidatePassport(passport, rules):
     for field in passport:
-        if field[0].strip() == 'byr' and ((1920 <= int(field[1]) <= 2002) == False): return False
-        elif field[0].strip() == 'iyr' and ((2010 <= int(field[1]) <= 2020) == False): return False
-        elif field[0].strip() == 'eyr' and ((2020 <= int(field[1]) <= 2030) == False):return False
-        elif field[0].strip() == 'hgt' and ((re.match(r'(^1[5-8][0-9]cm$|^19[0-3]cm$)|(^59in$|^6[0-9]in$|^7[0-6]in$)', field[1])) == None): return False
-        elif field[0].strip() == 'hcl' and ((re.match(r'^#[0-9a-f]{6}$', field[1])) == None): return False
-        elif field[0].strip() == 'ecl' and ((field[1] in ['amb','blu','brn','gry','grn','hzl','oth']) == False): return False
-        elif field[0].strip() == 'pid' and ((re.match(r'^\d{9}$', field[1])) == None): return False
+        try:
+            if re.match(rules[field[0]], field[1]) is None: return False
+        except KeyError:
+            continue
     return True
 
 passports = []
@@ -31,8 +24,17 @@ with open('day4input.txt') as f:
     for line in lines:
         passports.append([fields.split(':') for fields in line.split()])
 
-fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-print(CheckPassports(fields, passports))
-print(CheckPassports(fields, passports, True))
+requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+rules = {'byr': r'^(19[2-9][0-9]|200[0-2])$',
+         'iyr': r'^(201[0-9]|2020)$',
+         'eyr': r'^(202[0-9]|2030)$',
+         'hgt': r'^((1[5-8][0-9]cm|19[0-3]cm)|(59in|6[0-9]in|7[0-6]in))$',
+         'hcl': r'^#[0-9a-f]{6}$',
+         'ecl': r'^(amb|blu|brn|gry|grn|hzl|oth)$',
+         'pid': r'^\d{9}$'}
+
+
+print(CheckPassports(requiredFields, passports))
+print(CheckPassports(requiredFields, passports, rules, True))
 
 
