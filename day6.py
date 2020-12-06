604c9eaff3e291b1b0761ef640269fcf598e8ad7
Tyler Yanke <yanketyler@gmail.com>
2020-12-06 12:19:47 -0500

    Add files via upload

--- day6.py
+++ day6.py
@@ -4,11 +4,6 @@ def yesAnswerCount(answerGroup, unique=True):
     else: return len(set.intersection(*answerSet))
 
 answerGroups = [group.split('\n') for group in open('day6input.txt').read().split('\n\n')]
-uniqueYesAnsers = []
-allYesAnswers = []
-for answerGroup in answerGroups:
-    uniqueYesAnsers.append(yesAnswerCount(answerGroup))
-    allYesAnswers.append(yesAnswerCount(answerGroup, False))
 
-print(sum(uniqueYesAnsers))
-print(sum(allYesAnswers))
+print(sum([yesAnswerCount(group) for group in answerGroups]))
+print(sum([yesAnswerCount(group, False) for group in answerGroups]))
